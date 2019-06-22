d39a4c6682c560ed09e25b3c6f5963f51422a985
meharperjr <meharperjr@gmail.com>
2019-06-21 23:21:34 -0700

    final homework commit

--- Pybank/Main.py
+++ Pybank/Main.py
@@ -1,62 +1,63 @@
-#importing stuff
 import os
 import csv
 
-#importing CSV
-budget_date_csv = os.path.join("C:\\Users\\mehar\\Documents\\Python\\budget-data.csv")
-
-#creating output file
-outfile = os.path.join("..", "..", "Data", "PyBank_Financial_Summary.txt")
-
-months = []
-revenue = []
-
-with open("budget_date_csv", 'r') as csvfile:
-    csvread = csv.reader(csvfile)
-    
-    next(csvread, None)
-
-    for row in csvread:
-        months.append(row[0])
-        revenue.append(int(row[1]))
-
-#find total months
-
-total_months = len(months)
-
-#greatest increase, decrease variables
-#set to 0 
-greatest_inc = revenue[0]
-greatest_dec = revenue[0]
+month_count = 0
 total_revenue = 0
+this_month_revenue = 0
+last_month_revenue = 0
+revenue_change = 0
+revenue_changes = []
+months = []
 
-#loop through revenue indices and compare # to find greatest inc and dec
-#also add each revenue to total revenue
-for r in range(len(revenue)):
-    if revenue[r] >= greatest_inc:
-        greatest_inc = revenue[r]
-        great_inc_month = months[r]
-    elif revenue[r] <= greatest_dec:
-        greatest_dec = revenue[r]
-        great_dec_month = months[r]
-    total_revenue += revenue[r]
 
-#calculate average_change
-average_change = round(total_revenue/total_months, 2)
+# open csv file
+bank_csv = os.path.join("budget-data.csv")
+with open(bank_csv,'r', newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-#sets path for output file
-output_dest = os.path.join("C:\\Users\\mehar\\Documents\\Python\\pybank_output.txt")
+    next(csvreader)
 
-# opens the output destination in write mode and prints the summary
-with open(output_dest, 'w') as writefile:
-    writefile.writelines('Financial Analysis\n')
-    writefile.writelines('----------------------------' + '\n')
-    writefile.writelines('Total Months: ' + str(total_months) + '\n')
-    writefile.writelines('Total Revenue: $' + str(total_revenue) + '\n')
-    writefile.writelines('Average Revenue Change: $' + str(average_change) + '\n')
-    writefile.writelines('Greatest Increase in Revenue: ' + great_inc_month + ' ($' + str(greatest_inc) + ')'+ '\n')
-    writefile.writelines('Greatest Decrease in Revenue: ' + great_dec_month + ' ($' + str(greatest_dec) + ')')
+  
+    for row in csvreader:
+        month_count = month_count + 1
+        months.append(row[0])
+        this_month_revenue = int(row[1])
+        total_revenue = total_revenue + this_month_revenue
+        if month_count > 1:
+            revenue_change = this_month_revenue - last_month_revenue
+            revenue_changes.append(revenue_change)
+        last_month_revenue = this_month_revenue
 
-#opens the output file in r mode and prints to terminal
-with open(output_dest, 'r') as readfile:
-    print(readfile.read())
+
+# analyze the month by month results
+sum_rev_changes = sum(revenue_changes)
+average_change = sum_rev_changes / (month_count - 1)
+max_change = max(revenue_changes)
+min_change = min(revenue_changes)
+max_month_index = revenue_changes.index(max_change)
+min_month_index = revenue_changes.index(min_change)
+max_month = months[max_month_index]
+min_month = months[min_month_index]
+
+#Print to terminal
+
+print("Financial Analysis")
+print("----------------------------------------")
+print(f"Total Months: {month_count}")
+print(f"Total Revenue: ${total_revenue}")
+print(f"Average Revenue Change: ${average_change}")
+print(f"Greatest Increase in Revenue: {max_month} (${max_change})")
+print(f"Greatest Decrease in Revenue: {min_month} (${min_change})")
+
+
+#Print to file
+
+filepath = os.path.join("pybank_output.txt")
+with open(filepath,'w') as text:
+    text.write("Financial Analysis" + "\n")
+    text.write("----------------------------------------" + "\n")
+    text.write(f"Total Months: {month_count}" + "\n")
+    text.write(f"Total Revenue: ${total_revenue}" + "\n")
+    text.write(f"Average Revenue Change: ${average_change}" + "\n")
+    text.write(f"Greatest Increase in Revenue: {max_month} (${max_change})" + "\n")
+    text.write(f"Greatest Decrease in Revenue: {min_month} (${min_change})" + "\n")
